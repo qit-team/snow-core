151e21e48c19fe22b2dc180274cb55a59e5f116b
hetiansu5 <541919050@qq.com>
2019-07-24 17:53:22 +0800

    fix:http_query_build

--- utils/url.go
+++ utils/url.go
@@ -1,12 +1,15 @@
 package utils
 
-import "net/url"
+import (
+	"net/url"
+	"fmt"
+)
 
 //对map结构进行http_query_build
 func HttpBuildQuery(params map[string]interface{}) string {
 	v := url.Values{}
 	for key, value := range params {
-		v.Add(key, value.(string))
+		v.Add(key, fmt.Sprint(value))
 	}
 	return v.Encode()
 }
