f9fac56e9f4e4e8dc4e9811533ad44c742cbc19b
chenyilong <chenyilong@wlmkids.com>
2021-06-26 11:37:26 +0800

    feat: add skywalking to trace redis operation

--- cache/rediscache/redis_cache.go
+++ cache/rediscache/redis_cache.go
@@ -2,10 +2,11 @@ package rediscache
 
 import (
 	"context"
-	redis_pool "github.com/hetiansu5/go-redis-pool"
+	goredis "github.com/go-redis/redis/v8"
 	"github.com/qit-team/snow-core/cache"
 	"github.com/qit-team/snow-core/redis"
 	"sync"
+	"time"
 )
 
 var (
@@ -14,7 +15,7 @@ var (
 )
 
 type RedisCache struct {
-	client *redis_pool.ReplicaPool
+	client *goredis.Client
 }
 
 //实例模式
@@ -46,16 +47,15 @@ func GetRedisCache(diName string) cache.Cache {
  * 注意事项，如果key值不存在的话，返回的是空字符串，而不是nil
  */
 func (c *RedisCache) Get(ctx context.Context, key string) (interface{}, error) {
-	value, err := c.client.Get(key)
-	if err == redis_pool.ErrNil {
+	value, err :=  c.client.Get(ctx,key).Result()
+	if err == goredis.Nil {
 		return "", nil
 	}
 	return value, err
 }
 
 func (c *RedisCache) GetMulti(ctx context.Context, keys ...string) (map[string]interface{}, error) {
-	cKeys := convert(keys)
-	values, err := c.client.MGet(cKeys...)
+	values, err := c.client.MGet(ctx,keys...).Result()
 	if err != nil {
 		return nil, err
 	}
@@ -69,7 +69,11 @@ func (c *RedisCache) GetMulti(ctx context.Context, keys ...string) (map[string]i
 
 func (c *RedisCache) Set(ctx context.Context, key string, value interface{}, ttl ...int) (bool, error) {
 	t := cache.GetTTLOrDefault(ttl...)
-	return c.client.SetEX(key, value, int64(t))
+	_,err := c.client.SetEX(ctx,key, value, time.Duration(t)*time.Second).Result()
+	if err != nil {
+		return false,nil
+	}
+	return true,nil
 }
 
 func (c *RedisCache) SetMulti(ctx context.Context, items map[string]interface{}, ttl ...int) (bool, error) {
@@ -77,39 +81,38 @@ func (c *RedisCache) SetMulti(ctx context.Context, items map[string]interface{},
 	for key, value := range items {
 		arr = append(arr, key, value)
 	}
-	ok, err := c.client.MSet(arr...)
+	_, err := c.client.MSet(ctx,arr...).Result()
 	if err != nil {
-		return ok, err
+		return false, err
 	}
 
 	t := cache.GetTTLOrDefault(ttl...)
 	if t > 0 {
 		t64 := int64(t)
 		for key, _ := range items {
-			c.client.Expire(key, t64)
+			c.client.Expire(ctx,key, time.Duration(t64)*time.Second)
 		}
 	}
 	return true, nil
 }
 
 func (c *RedisCache) Delete(ctx context.Context, key string) (bool, error) {
-	res, err := c.client.Del(key)
+	res, err := c.client.Del(ctx,key).Result()
 	return res > 0, err
 }
 
 func (c *RedisCache) DeleteMulti(ctx context.Context, keys ...string) (bool, error) {
-	cKeys := convert(keys)
-	res, err := c.client.Del(cKeys...)
+	res, err := c.client.Del(ctx,keys...).Result()
 	return res > 0, err
 }
 
 func (c *RedisCache) Expire(ctx context.Context, key string, ttl ...int) (bool, error) {
 	t := cache.GetTTLOrDefault(ttl...)
-	return c.client.Expire(key, int64(t))
+	return c.client.Expire(ctx,key, time.Duration(t)*time.Second).Result()
 }
 
 func (c *RedisCache) IsExist(ctx context.Context, key string) (bool, error) {
-	num, err := c.client.Exists(key)
+	num, err := c.client.Exists(ctx,key).Result()
 	return num == 1, err
 }
 
@@ -122,19 +125,19 @@ func convert(keys []string) []interface{} {
 }
 
 func (c *RedisCache) IncrBy(ctx context.Context, key string, value int64) (int64, error) {
-	newVal, err := c.client.IncrBy(key, value)
+	newVal, err := c.client.IncrBy(ctx,key, value).Result()
 	if err != nil {
 		return 0, err
 	}
-	return int64(newVal), err
+	return newVal, err
 }
 
 func (c *RedisCache) DecrBy(ctx context.Context, key string, value int64) (int64, error) {
-	newVal, err := c.client.DecrBy(key, value)
+	newVal, err := c.client.DecrBy(ctx,key, value).Result()
 	if err != nil {
 		return 0, err
 	}
-	return int64(newVal), err
+	return newVal, err
 }
 
 func init() {
